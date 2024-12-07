113135df17eb4d2affe472fc111bb009097a1c25
JasperK04 <j.kleine.4@student.rug.nl>
2024-12-07 21:28:44 +0100

    add solution day 7

---
+++ 2024/day7/concatinate.py
@@ -0,0 +1,36 @@
+import sys
+import re
+
+def findTotal(target: int, current: int, numbers: list[int]) -> int:
+    if current == target:
+        return current
+    if not numbers or current > target:
+        return 0
+    temp = int(str(current) + str(numbers[0]))
+    value = findTotal(target, temp, numbers[1:])
+    if value:
+        return value
+    temp = current * numbers[0]
+    value = findTotal(target, temp, numbers[1:])
+    if value:
+        return value
+    temp = current + numbers[0]
+    value = findTotal(target, temp, numbers[1:])
+    if value:
+        return value
+    return 0
+
+
+def main():
+    sum = 0
+    data = sys.stdin.read().strip().split("\n")
+    for line in data:
+        total, *numbers = re.findall(r"[0-9]+" ,line)
+        numbers = [int(num) for num in numbers]
+        sum += findTotal(int(total), numbers[0], numbers[1:])
+    
+    print(sum)
+
+
+if __name__ == "__main__":
+    main()
